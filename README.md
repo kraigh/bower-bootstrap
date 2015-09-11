6fa2572f2cc263aebb963caf07822fc716462ebb
kraigh <kraigory@gmail.com>
2015-09-11 17:59:23 -0500

    got everything going i think except for jade

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bootstrapping Bootstrap / Bower / Jade
+## Installation:
+* Note: If you're using Mac OSX, many of these installs will be easier with [Homebrew]().
+1. You're going to need [nodejs](https://nodejs.org/en/download/) installed.
+    * Installing with Homebrew: `brew install node`
+2. Make sure you have [ruby](https://www.ruby-lang.org/en/documentation/installation/) installed. `ruby -v` will tell you if you have it installed or not.
+    * Installing with Homebrew: `brew install ruby`
+3. In the project root, run `npm install`
+    * This will install gulp and all its dependencies
+4. In the project root, run `bower install`
+    * This will install Bootstrap and Font Awesome
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,65 @@
-// Basic Gulp File
-//
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass')
-    autoprefix = require('gulp-autoprefixer')
-    notify = require("gulp-notify")
-    bower = require('gulp-bower');
- 
+var gulp = require('gulp'), 
+    sass = require('gulp-ruby-sass') ,
+    notify = require("gulp-notify") ,
+    filesize = require('gulp-size'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
+
 var config = {
-    sassPath: './resources/sass',
-    bowerDir: './bower_components'
-}
- 
-gulp.task('bower', function() {
-    return bower()
-        .pipe(gulp.dest(config.bowerDir))
+     sassPath: './resources/sass',
+    bowerDir: './bower_components' 
+};
+
+var scripts = [
+    config.bowerDir + '/jquery/dist/jquery.js',
+    config.bowerDir + '/bowser/bowser.js',
+    config.bowerDir + '/jquery.easing/js/jquery.easing.js',
+    config.bowerDir + '/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+    './resources/js/*.js'
+];
+
+gulp.task('icons', function() { 
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
+        .pipe(gulp.dest('./public/fonts')); 
 });
- 
-gulp.task('icons', function() {
-    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
-        .pipe(gulp.dest('./public/fonts'));
+
+gulp.task('css', function() { 
+    return sass(config.sassPath + '/style.scss', {
+        style: 'compressed',
+         loadPath: [
+             config.sassPath + '/partials',
+             config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+            config.bowerDir + '/bootstrap-material-design/sass',
+            config.bowerDir,
+         ],
+        sourcemap: true
+         }) 
+        .on("error", notify.onError(function (error) {
+            return "Error: " + error.message;
+        }))
+        .pipe(gulp.dest('./public/css'))
+        .pipe(notify({ message: 'Page styles compiled.' }));
 });
- 
-gulp.task('css', function() {
-    return gulp.src(config.sassPath + '/style.scss')
-        .pipe(sass({
-            style: 'compressed',
-            loadPath: [
-                './resources/sass',
-                config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
-                config.bowerDir + '/fontawesome/scss',
-            ]
+
+gulp.task('scripts', function() {
+    return gulp.src(scripts)
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .on('error', function(err){
+            console.log(err);
         })
-            .on("error", notify.onError(function (error) {
-                return "Error: " + error.message;
-            })))
-        .pipe(autoprefix('last 2 version'))
-        .pipe(gulp.dest('./public/css'));
+        .pipe(gulp.dest('public/js/'))
+        .pipe(notify({ message: 'all.js concated' }))
+        .pipe(filesize({ showFiles: true }));
 });
- 
+
 // Rerun the task when a file changes
-gulp.task('watch', function() {
-    gulp.watch(config.sassPath + '/**/*.scss', ['css']);
+ gulp.task('watch', function() {
+    // Watch .scss files
+     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+    gulp.watch(config.sassPath + '/partials/*.scss', ['css']); 
+    // Watch .js files
+    gulp.watch('resources/js/*.js', ['scripts']);
 });
- 
-gulp.task('default', ['bower', 'icons', 'css']);
+
+  gulp.task('default', ['icons', 'css', 'scripts']);
